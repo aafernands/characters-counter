c1c7b18b2e2bccdfbec6a3489a8a13179ae9cbef
Alex Fernandes <alexfernands@outlook.com>
2020-09-29 12:36:43 -0400

    Included functions

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var tweet = prompt('Write a message:');
-
 tweetCount = tweet.length;
 tweetLength = 148;
 YourMessage = tweet;
 
-alert('You have written ' + tweet.length + ' characters, you have ' + (148 - tweet.length) + ' characters remaining.');
+function myFunction() {
+	var tweet = prompt('Write a message:');
+	alert(`You have written ${tweet.length} characters, you have ${148 - tweet.length} characters remaining.`);
 
-document.getElementById('YourMessage').innerHTML = YourMessage;
+	document.getElementById('YourMessage').innerHTML = YourMessage;
 
-document.getElementById('tweetCount').innerHTML = tweetCount;
+	document.getElementById('tweetCount').innerHTML = tweetCount;
 
-document.getElementById('remainingCharacters').innerHTML = tweetCount - 148;
+	document.getElementById('remainingCharacters').innerHTML = tweetCount - 148;
 
-document.getElementById('tweetLength').innerHTML = tweetLength;
+	document.getElementById('tweetLength').innerHTML = tweetLength;
+}
